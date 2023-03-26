e269695d4a6a0cfffc4a07f6a80ae5a9a53e346e
rosemutai <chepngetichrose2030@gmail.com>
2023-03-26 17:05:16 +0300

    Add filter apartment function

--- script.js
+++ script.js
@@ -2,17 +2,37 @@
 const form = document.getElementById('navForm');
 const formSection = document.getElementById('formSection');
 const editClose = document.getElementById('editClose');
+let searchedLocation = document.getElementById('location-input');
+const guests = document.getElementById('guests-input');
 
-form.addEventListener('submit', (e) => {
 
-  let searchBtn = document.getElementById('searchBtn');
-  let searchedLocation = document.getElementById('location-input').value.toLowerCase();
-  let guests = Number(document.getElementById('guests-input').value);
-
-  e.preventDefault();
-  console.log(searchedLocation);
-  console.log(guests);
+searchedLocation.addEventListener('click', () => {
   formSection.classList.add('active');
   searchBtn.classList.add('activebutton');
   editClose.style.display = 'block';
+  
 });
+
+
+const searchStays = () => {
+
+  fetch('./stays.json')
+    .then(data => data.json())
+    .then(results => {
+
+      const filteredData = results.filter((item) => {
+        const cityMatch = searchedLocation.value ? item.city.toLowerCase().includes(searchedLocation.value.toLowerCase()) : true;
+        const maxGuestsMatch = guests.value ? item.maxGuests == guests.value : true;
+        return cityMatch && maxGuestsMatch;
+      });
+      console.log(filteredData);
+
+    })
+  
+
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchStays();
+})
