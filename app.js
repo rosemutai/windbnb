37b3ac144d2d735ec7be8d9430462bd1f704e661
rosemutai <chepngetichrose2030@gmail.com>
2023-04-02 17:42:19 +0300

    Modify stays

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ const apartmentsSection = document.getElementById('apartments-section');
 
 
 
-    stays.forEach((apartment) => {
-      const cardContents = `
-            <img src=${apartment.photo} alt="">
-            <div class="description">
-              ${apartment.superHost ? '<button class="superhost">SUPER HOST</button>' : ''}
-              <p class="entire-apartment">
-                ${apartment.type}
-                <span class="beds">. ${apartment.beds} beds</span>
-              </p>
-            </div>
-            <div class="name">
-              <p class='title'>${apartment.title}</p>
-            </div>
-      `;
-      const cardDiv = document.createElement('div');
-      cardDiv.innerHTML = cardContents;
-      cardDiv.classList.add('card')
-      apartmentsSection.append(cardDiv);
+stays.forEach((apartment) => {
+  const cardContents = `
+    <img src=${apartment.photo} alt="">
+    <div class="description">
+      ${apartment.superHost ? '<button class="superhost">SUPER HOST</button>' : ''}
+      <p class="entire-apartment">
+        ${apartment.type}
+        <span class="beds">. ${apartment.beds} beds</span>
+      </p>
+    </div>
+    <div class="name">
+      <p class='title'>${apartment.title}</p>
+    </div>
+  `;
+  const cardDiv = document.createElement('div');
+  cardDiv.innerHTML = cardContents;
+  cardDiv.classList.add('card')
+  apartmentsSection.append(cardDiv);
 
-    })
-  
+})
