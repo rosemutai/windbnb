d7a4c0fca00107f617117c34bc2956e40f1053cf
rosemutai <chepngetichrose2030@gmail.com>
2023-04-02 16:43:45 +0300

    Modify source of data

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+import stays from './stays.js'
 const apartmentsSection = document.getElementById('apartments-section');
 
-fetch('./stays.json')
-  .then((apartments) => apartments.json())
-  .then((Apartments) => {
-    Apartments.forEach((apartment) => {
+
+
+    stays.forEach((apartment) => {
       const cardContents = `
             <img src=${apartment.photo} alt="">
             <div class="description">
@@ -12,12 +12,6 @@ fetch('./stays.json')
                 ${apartment.type}
                 <span class="beds">. ${apartment.beds} beds</span>
               </p>
-              <div class="rating">
-                <p class="rating-num">
-                  <i class="material-icons star-icon">star</i>
-                  <span class="rate">${apartment.rating}</span>
-                </p>
-              </div>
             </div>
             <div class="name">
               <p class='title'>${apartment.title}</p>
@@ -29,5 +23,4 @@ fetch('./stays.json')
       apartmentsSection.append(cardDiv);
 
     })
-  }
-)
+  
